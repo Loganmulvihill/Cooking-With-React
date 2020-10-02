a7a78fd9178aef7af7c44eb4122474ab9920e050
loganmulvihill <mulvihill.logan@gmail.com>
2020-10-01 22:37:02 -0500

    dynamically rendered data into RecipeList through map method

--- src/App.js
+++ src/App.js
@@ -4,9 +4,26 @@ import RecipeList from './RecipeList';
 function App() {
   return(
 
-    <RecipeList />
+    <RecipeList recipes={sampleRecipes} />
 
   )
 }
+
+const sampleRecipes = [
+  {
+    id:1,
+    name: 'Plain Chicken',
+    servings: 3,
+    cookTime: "1:45",
+    instructions: "1. Put salt on Chicken\n2. Put chicken in oven\n3. Eat the chicken"
+  },
+  {
+    id: 2,
+    name: 'Plain Pork',
+    servings: 5,
+    cookTime: "0:45",
+    instructions: "1. Put paprika on pork\n2. Put pork in oven\n3. Eat the pork"
+  },
+]
 
 export default App;
