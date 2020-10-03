12bf08b345a7970a42f73a54fa4bb2c7dda45004
loganmulvihill <mulvihill.logan@gmail.com>
2020-10-02 23:21:27 -0500

    created ingredientList component

---
+++ src/ingredientList.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function ingredientList( { ingredients }) {
+const ingredientElements = ingredients.map(ingredient => {
+  return <Ingredient />
+})
+  return (
+    <div>
+
+    </div>
+  )
+}
