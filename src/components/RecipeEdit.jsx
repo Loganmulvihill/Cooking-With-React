962ae5a08670a312abe30de84de90bdd10aaaa78
loganmulvihill <mulvihill.logan@gmail.com>
2020-10-04 18:06:10 -0500

    added RecipeEdit component

---
+++ src/components/RecipeEdit.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+export default function RecipeEdit() {
+  return (
+    <div className="recipe-edit">
+      <div>
+        <button>&times;</button>
+      </div>
+      <div>
+        <label htmlFor="name">Name</label>
+        <input type="text" name="name" id="name" />
+        <label htmlFor="cookTime">Cook Time</label>
+        <input type="text" name="cookTime" id="cookTime" />
+        <label htmlFor="servings">Servings</label>
+        <input type="number" min='1' name="servings" id="servings" />
+        <label htmlFor="instructions">Instructions</label>
+        <textarea name="instructions" id="instructions"></textarea>
+      </div>
+      <br/>
+      <label>Ingredients</label>
+    </div>
+  )
+}
